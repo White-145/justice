74813f48eedce26f2f9a56f339cecd51dbbe1cb0
White_145 <white145gaming@gmail.com>
2025-09-03 11:46:20 +0400

    finish up the executable, sins easter egg

--- src/main/java/me/white/justice/JustIce.java
+++ src/main/java/me/white/justice/JustIce.java
@@ -6,38 +6,38 @@ import org.apache.commons.cli.help.HelpFormatter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.Scanner;
 
 public class JustIce {
     private static final Options COMMAND_LINE_OPTIONS = new Options();
-    private static final Option HELP_OPTION = Option.builder()
+    private static final Option HELP_OPTION = Option.builder("h")
             .desc("Show this help screen")
-            .option("h")
             .longOpt("help")
             .get();
-    private static final Option OUT_OPTION = Option.builder()
+    private static final Option OUT_OPTION = Option.builder("o")
             .desc("Specify the output file")
-            .option("o")
             .hasArg()
-            .type(File.class)
+                .type(File.class)
+                .argName("file")
             .get();
-    private static final Option PARSE_OPTION = Option.builder()
+    private static final Option PARSE_OPTION = Option.builder("p")
             .desc("Parse provided files")
-            .option("p")
             .longOpt("parse")
             .get();
-    private static final Option MARSHAL_OPTION = Option.builder()
+    private static final Option MARSHAL_OPTION = Option.builder("m")
             .desc("Marshal provided files")
-            .option("m")
             .longOpt("marshal")
             .get();
-    private static final Option COMPACT_OPTION = Option.builder()
-            .desc("Pack module into a single line")
-            .option("p")
-            .longOpt("pack")
+    private static final Option COMPACT_OPTION = Option.builder("c")
+            .desc("Compact module into a single line")
+            .longOpt("compact")
             .get();
+    private static final Option DRY_OPTION = Option.builder("d")
+            .desc("Do a dry run without producing an output file")
+            .longOpt("dry")
+            .get();
+    private static final Option SIN_OPTION = Option.builder("s").get();
     private final CommandLine commandLine;
 
     static {
@@ -48,6 +48,8 @@ public class JustIce {
                 .addOption(MARSHAL_OPTION)
         );
         COMMAND_LINE_OPTIONS.addOption(COMPACT_OPTION);
+        COMMAND_LINE_OPTIONS.addOption(DRY_OPTION);
+        COMMAND_LINE_OPTIONS.addOption(SIN_OPTION);
     }
 
     private JustIce(CommandLine commandLine) {
@@ -61,8 +63,7 @@ public class JustIce {
             JustIce program = new JustIce(commandLine);
             program.start();
         } catch (ParseException e) {
-            System.err.print("Could not parse command line arguments: ");
-            e.printStackTrace();
+            System.err.print("Could not parse command line arguments: " + e);
         }
     }
 
@@ -84,9 +85,13 @@ public class JustIce {
         return true;
     }
 
-    private void start() throws org.apache.commons.cli.ParseException {
+    private void start() throws ParseException {
         if (commandLine.hasOption(HELP_OPTION)) {
             help();
+            return;
+        }
+        if (commandLine.hasOption(SIN_OPTION)) {
+            sin();
             return;
         }
         String[] files = commandLine.getArgs();
@@ -99,8 +104,7 @@ public class JustIce {
             try {
                 paths[i] = Path.of(files[i]);
             } catch (InvalidPathException e) {
-                System.err.println("Invalid file path '" + files[i] + "': ");
-                e.printStackTrace();
+                System.err.print("Invalid file path '" + files[i] + "': " + e);
                 return;
             }
         }
@@ -119,8 +123,75 @@ public class JustIce {
         try {
             HelpFormatter.builder().setShowSince(false).get().printHelp("justice", null, COMMAND_LINE_OPTIONS, null, true);
         } catch (IOException e) {
-            System.err.print("Could not display help message: ");
-            e.printStackTrace();
+            System.err.print("Could not display help message: " + e);
+        }
+    }
+
+    private void sin() {
+        String[] sins = new String[]{
+                "WRATH",
+                "GREED",
+                "ENVY",
+                "LUST",
+                "SLOTH",
+                "PRIDE",
+                "GLUTTONY"
+        };
+        String[] praise = new String[]{
+                "How Righteous.",
+                "How Insatiable.",
+                "How Inspiring.",
+                "How Tender.",
+                "How Restful.",
+                "How Noble.",
+                "How Delectable."
+        };
+        String[] motive = new String[]{
+                "My fury is justified. My anger is pure.",
+                "I only want what's mine—and theirs, and his, and yours.",
+                "I see what others have, and I ache to take it for myself.",
+                "I long for touch—to feel alive again.",
+                "The world moves too fast. I crave stillness, and I want no part in its frenzy.",
+                "I've carved myself from stone. I bow to no one.",
+                "This world has prepared me a vast banquet, and I would be fool to not partake of it."
+        };
+        String[] truth = new String[]{
+                "You wear vengeance like armor, but it corrodes from within. You will become what you aimed to destroy.",
+                "The more you take, the less you have. You will starve surrounded by gold.",
+                "Your gaze lingers too long on others' light. In time, you will forget how to kindle your own.",
+                "You seek warmth in a fleeting fire. It will burn you hollow and leave you cold.",
+                "Your apathy will rot your will. Even the soul needs motion, lest it wither in place.",
+                "Even statues crack, and when you fall, no one will dare to catch you.",
+                "And when the beasts' flesh runs scarce, you would brandish your fangs against your fellow man, beast too that you've become."
+        };
+        System.out.print("What sin do you relish the most? [" + String.join("/", sins) + "]: ");
+        Scanner scanner = new Scanner(System.in);
+        String sin;
+        dialogue: while (true) {
+            sin = scanner.nextLine();
+            for (String option : sins) {
+                if (sin.equalsIgnoreCase(option)) {
+                    break dialogue;
+                }
+            }
+            System.out.print("[WRATH/GREED/ENVY/LUST/SLOTH/PRIDE/GLUTTONY]: ");
+        }
+        int idx;
+        for (idx = 0; idx < sins.length; ++idx) {
+            if (sin.equalsIgnoreCase(sins[idx])) {
+                break;
+            }
+        }
+        try {
+            Thread.sleep(2000);
+            System.out.println(praise[idx]);
+            Thread.sleep(3000);
+            System.out.println("\"" + motive[idx] + "\"");
+            Thread.sleep(5000);
+            System.out.println(truth[idx]);
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            throw new AssertionError(e);
         }
     }
 
@@ -131,24 +202,25 @@ public class JustIce {
         Parser parser = new Parser();
         parser.setCompact(commandLine.hasOption(COMPACT_OPTION));
         for (Path path : paths) {
-            try {
-                String source = Files.readString(path);
-                parser.parse(source);
-            } catch (IOException e) {
-                System.err.print("Could not read file '" + path + "': ");
-                e.printStackTrace();
-                return;
-            } catch (ParsingException e) {
-                System.out.println("Compile error: " + e.getMessage());
-                e.printStackTrace();
+            String source = readFile(path);
+            if (source == null) {
                 return;
             }
+            try {
+                parser.parse(source);
+            } catch (ParsingException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        if (commandLine.hasOption(DRY_OPTION)) {
+            System.out.println("Dry run success!");
+            return;
         }
         try (FileWriter file = new FileWriter(out)) {
             parser.write(file);
         } catch (IOException e) {
-            System.err.print("Could not write file '" + out + "': ");
-            e.printStackTrace();
+            System.err.print("Could not write file '" + out + "': " + e);
         }
     }
 
@@ -158,22 +230,36 @@ public class JustIce {
         }
         Marshal marshal = new Marshal();
         for (Path path : paths) {
-            try {
-                String source = Files.readString(path);
-                marshal.marshal(source);
-            } catch (IOException e) {
-                System.err.print("Could not read file '" + path + "': ");
-                e.printStackTrace();
+            String source = readFile(path);
+            if (source == null) {
                 return;
-            } catch (MarshalException e) {
-                System.out.println("Marshal error: " + e.getMessage());
             }
+            try {
+                marshal.marshal(source);
+            } catch (MarshalException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        if (commandLine.hasOption(DRY_OPTION)) {
+            System.out.println("Dry run success!");
+            return;
         }
         try (FileWriter writer = new FileWriter(out)) {
             marshal.write(writer);
         } catch (IOException e) {
-            System.err.print("Could not write file '" + out + "': ");
-            e.printStackTrace();
+            System.err.print("Could not write file '" + out + "': " + e);
         }
+    }
+
+    public static String readFile(Path path) {
+        try {
+            return Files.readString(path);
+        } catch (NoSuchFileException e) {
+            System.err.println("File '" + path + "' does not exist");
+        } catch (IOException e) {
+            System.err.println("Could not read file '" + path + "': " + e);
+        }
+        return null;
     }
 }
